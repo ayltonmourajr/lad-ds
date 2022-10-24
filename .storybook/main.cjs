1a4ec4f6f6912c7703ade702c3e477d0beb7e122
ayltonmourajr <100725781+ayltonmourajr@users.noreply.github.com>
2022-10-24 20:25:22 -0300

    ci: Add publish storybook workflow

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -14,5 +14,12 @@ module.exports = {
   },
   "features": {
     "storyStoreV7": true
+  },
+
+  viteFinal: (config, { configtype }) => {
+    if (configtype === 'PRODUTION') {
+      config.base = '/lad-ds/'
+    }
+    return config
   }
 }
